5a7d96102dec9a06eab3ca5cbee7f05a56eadd8f
AlexPi <alex.pigida@outlook.com>
2021-02-01 22:30:25 -0500

    fixed all

--- Src/UWP/RdpSupport/MainPage.xaml.cs
+++ Src/UWP/RdpSupport/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.System.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -11,12 +12,13 @@ namespace RdpSupport
     DisplayRequest _disp = new DisplayRequest();
     DateTime _since;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+    public MainPage()
+    {
+      InitializeComponent();
+      CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+    }
 
-        async void Page_Loaded(object s, RoutedEventArgs e) { await Task.Delay(999); onStart(s, e); }
+    async void Page_Loaded(object s, RoutedEventArgs e) { await Task.Delay(999); onStart(s, e); }
 
     void onStart(object s, RoutedEventArgs e) { _disp.RequestActive(); checkBox.IsChecked = true; dd(checkBox.IsChecked == true); textBlock.Text = $"Since {(_since = DateTime.Now):HH:mm}"; }
     void onStop(object se, RoutedEventArgs e) { _disp.RequestRelease(); checkBox.IsChecked = false; dd(checkBox.IsChecked == true); textBlock.Text = $"For {(DateTime.Now - _since):hh\\:mm}"; }
