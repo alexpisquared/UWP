e962d95e61b4f709bdda5a44debdd583d4654a5a
AlexPi <alex.pigida@outlook.com>
2021-02-04 08:52:48 -0500

    mandatory cut off time

--- Src/UWP/RdpSupport/MainPage.xaml.cs
+++ Src/UWP/RdpSupport/MainPage.xaml.cs
@@ -8,35 +8,45 @@ using Windows.UI.Xaml.Controls;
 
 namespace RdpSupport
 {
-  public sealed partial class MainPage : Page
-  {
-    readonly DisplayRequest _dr = new DisplayRequest();
-    DateTime _since;
-
-    public MainPage()
+    public sealed partial class MainPage : Page
     {
-      InitializeComponent();
-      CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+        readonly DisplayRequest _dr = new DisplayRequest();
+        DateTime _since;
+        DispatcherTimer _timer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            _timer.Tick += _timer_Tick;
+            _timer.Interval = TimeSpan.FromSeconds(90);
+            _timer.Start();
+        }
+
+        void _timer_Tick(object sender, object e)
+        {
+            if (DateTime.Now.Hour >= 20 && checkBox.IsChecked == true)
+                setDR(false);
+        }
+
+        async void Page_Loaded(object s, RoutedEventArgs e) { await Task.Delay(999); onStart(s, e); }
+
+        void onStart(object s, RoutedEventArgs e) => setDR(true);
+        void onStop(object se, RoutedEventArgs e) => setDR(false);
+
+        void setDR(bool isOn)
+        {
+            checkBox.IsChecked = isOn;
+            if (isOn)
+                _dr.RequestActive();
+            else
+                _dr.RequestRelease();
+
+            textBlock.Text = isOn ? $"On since {(_since = DateTime.Now):HH:mm}" : $"Was On for {(DateTime.Now - _since):hh\\:mm}";
+            ApplicationView.GetForCurrentView().Title = isOn ? $"{(_since = DateTime.Now):HH:mm}···" : $"Off";
+
+            //buttonStart.Visibility = v ? Visibility.Collapsed : Visibility.Visible;
+            //buttonStop.Visibility = !v ? Visibility.Collapsed : Visibility.Visible;
+        }
     }
-
-    async void Page_Loaded(object s, RoutedEventArgs e) { await Task.Delay(999); onStart(s, e); }
-
-    void onStart(object s, RoutedEventArgs e) => setDR(true);
-    void onStop(object se, RoutedEventArgs e) => setDR(false);
-
-    void setDR(bool isOn)
-    {
-      checkBox.IsChecked = isOn;
-      if (isOn)
-        _dr.RequestActive();
-      else
-        _dr.RequestRelease();
-
-      textBlock.Text = isOn ? $"On since {(_since = DateTime.Now):HH:mm}" : $"Was On for {(DateTime.Now - _since):hh\\:mm}";
-      ApplicationView.GetForCurrentView().Title = isOn ? $"{(_since = DateTime.Now):HH:mm}···" : $"Off";
-
-      //buttonStart.Visibility = v ? Visibility.Collapsed : Visibility.Visible;
-      //buttonStop.Visibility = !v ? Visibility.Collapsed : Visibility.Visible;
-    }
-  }
 }
